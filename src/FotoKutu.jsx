c7ceb1c510d7b01d0103df8fe160d4a2487ecfd5
Batuhan Yerlikaya <batuhanyky@hotmail.com>
2023-03-12 20:30:02 +0300

    feat:API ile deneme yapıldı

---
+++ src/FotoKutu.jsx
@@ -0,0 +1,13 @@
+import "./foto-kutu.css"
+function FotoKutu(ozellikler) {
+
+    return (
+        <div className="foto-kutu">
+            <img src={ozellikler.url} alt="" />
+            <h2>{ozellikler.sahibi}</h2>
+            <button>Paylaş</button>
+        </div>
+    )
+}
+
+export default FotoKutu
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import FotoKutu from "./FotoKutu.jsx"
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <section>
-    <p>App 1</p>
+  <section id='foto-liste'>
+
+    <FotoKutu url="https://picsum.photos/id/0/250/150" sahibi="Alejandro Escamilla" />
+    <FotoKutu url="https://picsum.photos/id/1/250/150" sahibi="Lorem Ipsum" />
+    <FotoKutu url="https://picsum.photos/id/2/250/150" sahibi="Dolor Sit" />
+    <FotoKutu url="https://picsum.photos/id/3/250/150" sahibi="Müslüm Gürses" />
+    <FotoKutu url="https://picsum.photos/id/4/250/150" sahibi="Ferdi Tayfur" />
+    <FotoKutu url="https://picsum.photos/id/5/250/150" sahibi="Orhan Gencebay" />
+
   </section>
 );
+
