743c0f5d6f3a2e406e29065a0d180ec7dd0aceb3
Batuhan Yerlikaya <batuhanyky@hotmail.com>
2023-03-12 17:10:17 +0300

    feat:css ekleme yolları gösterildi.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <section>
-    <p>App 1</p>
+  <section className='container'>
+    <p className='kirmizi-yazi'>App 1</p>
   </section>
 );
