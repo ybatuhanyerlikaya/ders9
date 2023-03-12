42d9ac15ca0e01be6e6d0f8c1e5a8ac3ae18f913
Batuhan Yerlikaya <batuhanyky@hotmail.com>
2023-03-12 19:44:06 +0300

    feat:Genel UI oluşturuldu

---
+++ src/ModKutusu.jsx
@@ -0,0 +1,16 @@
+import "./mod-kutusu.css"
+
+function ModKutusu(ozellikler) {
+
+
+    return (
+
+        <div className="mod-kutusu">
+            <img src={ozellikler.url} alt={ozellikler.alt} />
+            <h2>{ozellikler.modAdi}</h2>
+        </div>
+
+    )
+}
+
+export default ModKutusu //Komponenti dışarıya açık hale getirdik
